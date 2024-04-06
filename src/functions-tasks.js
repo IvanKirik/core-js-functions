a2aafcb70aa1c10a1602a8d87bc2c77924bc6a59
Ivan Haurylenka <ivankirikgavrilenko@gmail.com>
2024-04-06 13:01:35 +0300

    feat:completed retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -136,8 +136,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function fn() {
+    let attempt = 0;
+    while (attempt < attempts) {
+      try {
+        return func();
+      } catch (error) {
+        attempt += 1;
+      }
+    }
+    throw new Error('All attempts failed');
+  };
 }
 
 /**
